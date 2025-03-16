79e9bbde3f1b82aa908c39b8592b04774b566529
현지현 <qwere4585@naver.com>
2025-03-16 18:46:20 +0900

    chore: init project

---
+++ chaeum-fe/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: '채움',
+  description: '',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
