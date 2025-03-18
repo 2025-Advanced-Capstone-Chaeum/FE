d0d90a9a738dc0a40411ac746d70be054780cce9
현지현 <qwere4585@naver.com>
2025-03-18 23:47:28 +0900

    refactor: 네비바 사이즈 조정

--- src/components/ui/NavigationBar.tsx
+++ src/components/ui/NavigationBar.tsx
@@ -4,43 +4,13 @@ import React, { useState } from "react";
 import Image from "next/image";
 
 const NavigationBar = () => {
-  const [isActiveMenu, setIsActiveMenu] = useState(0);
-  const navItemsLeft = [
-    {
-      id: 1,
-      src: "/assets/icons/funding.svg",
-      alt: "Funding",
-      label: "펀딩",
-      width: 26,
-      height: 27,
-    },
-    {
-      id: 2,
-      src: "/assets/icons/review.svg",
-      alt: "Review",
-      label: "후기",
-      width: 28,
-      height: 28,
-    },
-  ];
+  const [isActiveMenu, setIsActiveMenu] = useState<number>(0);
 
-  const navItemsRight = [
-    {
-      id: 3,
-      src: "/assets/icons/friend.svg",
-      alt: "Friend",
-      label: "친구",
-      width: 25,
-      height: 25,
-    },
-    {
-      id: 4,
-      src: "/assets/icons/profile.svg",
-      alt: "Profile",
-      label: "내 계정",
-      width: 29,
-      height: 29,
-    },
+  const navItems = [
+    { id: 1, src: "/assets/icons/funding.svg", alt: "Funding", label: "펀딩", width: 24, height: 25 },
+    { id: 2, src: "/assets/icons/review.svg", alt: "Review", label: "후기", width: 25, height: 25 },
+    { id: 3, src: "/assets/icons/friend.svg", alt: "Friend", label: "친구", width: 23, height: 23 },
+    { id: 4, src: "/assets/icons/profile.svg", alt: "Profile", label: "내 계정", width: 25, height: 25 },
   ];
 
   const handleMenuClick = (id: number) => {
@@ -48,16 +18,16 @@ const NavigationBar = () => {
   };
 
   return (
-    <div className="w-full h-[4.5rem] fixed bottom-0 bg-white">
+    <div className="w-full h-[4rem] fixed bottom-0 bg-white">
       <div
-        className="absolute top-1 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[80px] h-[80px] rounded-full flex items-center justify-center cursor-pointer bg-primary opacity-80"
+        className="absolute top-[5px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[70px] h-[70px] rounded-full flex items-center justify-center cursor-pointer bg-primary opacity-80"
         onClick={() => handleMenuClick(0)}
       >
-        <Image src="/assets/icons/home.svg" alt="Home" width={28} height={28} />
+        <Image src="/assets/icons/home.svg" alt="Home" width={25} height={26} />
       </div>
       <div className="flex w-full">
-        <div className="flex w-[45%] pl-12 pt-3 justify-between">
-          {navItemsLeft.map((item) => (
+        <div className="flex w-[45%] pl-12 pt-2 justify-between">
+        {navItems.slice(0, 2).map((item) => (
             <div
               key={item.id}
               className="flex gap-0.5 items-center justify-center cursor-pointer"
@@ -65,44 +35,30 @@ const NavigationBar = () => {
             >
               <div
                 className={`flex flex-col items-center justify-center relative ${
-                  isActiveMenu === item.id ? "opacity-50" : "opacity-100"
+                  isActiveMenu === item.id ? "opacity-30" : "opacity-100"
                 }`}
               >
-                <Image
-                  src={item.src}
-                  alt={item.alt}
-                  width={item.width}
-                  height={item.height}
-                />
-              <span className="font-semibold text-xs text-primary">
-                {item.label}
-              </span>
+                <Image src={item.src} alt={item.alt} width={item.width} height={item.height} />
+                <span className="font-semibold text-xs text-primary">{item.label}</span>
               </div>
             </div>
           ))}
         </div>
         <div className="flex w-full justify-end">
-          <div className="flex w-1/2 pr-12 pt-3 justify-between">
-            {navItemsRight.map((item) => (
+          <div className="flex w-1/2 pr-12 pt-2 justify-between">
+          {navItems.slice(2, 4).map((item) => (
               <div
                 key={item.id}
                 className="flex gap-0.5 items-center justify-center cursor-pointer"
                 onClick={() => handleMenuClick(item.id)}
               >
                 <div
-                className={`flex flex-col items-center justify-center relative ${
-                  isActiveMenu === item.id ? "opacity-50" : "opacity-100"
-                }`}
-              >
-                <Image
-                  src={item.src}
-                  alt={item.alt}
-                  width={item.width}
-                  height={item.height}
-                />
-                <span className="font-semibold text-xs text-primary">
-                  {item.label}
-                </span>
+                  className={`flex flex-col items-center justify-center relative ${
+                    isActiveMenu === item.id ? "opacity-30" : "opacity-100"
+                  }`}
+                >
+                  <Image src={item.src} alt={item.alt} width={item.width} height={item.height} />
+                  <span className="font-semibold text-xs text-primary">{item.label}</span>
                 </div>
               </div>
             ))}
