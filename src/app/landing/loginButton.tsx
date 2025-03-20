98190042a5558b24c1b9073ebeb0561c1f17d713
aeri0730 <dodl8549@gmail.com>
2025-03-20 21:59:29 +0900

    Refactor : 랜딩페이지 경로 변경 (#14)

--- src/app/landing/loginButton.tsx
+++ src/app/landing/loginButton.tsx
@@ -1,45 +1,46 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { baseButtonStyles } from "@/styles/styles";
+import clsx from "clsx";
 import Image from "next/image";
-import { SiNaver } from "react-icons/si";
-import kakaoLogo from "../../public/assets/image/kakaoLogo.png";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { SiNaver } from "react-icons/si";
+import kakaoLogo from "../../../public/assets/image/카카오.png";
 
 export default function LoginButtons() {
   const [showButtons, setShowButtons] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
-    // 4초 후 로그인인 버튼 표시
-    const buttonTimer = setTimeout(() => {
-      setShowButtons(true);
+    const token = localStorage.getItem("token");
+
+    // 4초 후 실행 로직
+    const timer = setTimeout(() => {
+      if (token) {
+        setIsAuthenticated(true);
+        router.push("/");
+      } else {
+        setShowButtons(true);
+      }
     }, 4000);
 
-    // JWT 체크 (기존 로그인 여부)
-    const token = localStorage.getItem("token");
-    if (token) {
-      setIsAuthenticated(true);
-      return;
-    }
-
-    return () => clearTimeout(buttonTimer);
+    return () => clearTimeout(timer);
   }, []);
 
   if (!showButtons) {
     return null;
   }
-  if (!showButtons && isAuthenticated) {
-    return (
-      <div>
-        <div>메인페이지</div>
-      </div>
-    );
-  }
 
   return (
     <div className="absolute bottom-24 flex flex-col space-y-4 transition-opacity duration-1000 opacity-100">
-      <Button className="w-80 h-16 bg-yellow-300 hover:bg-yellow-600 text-black relative rounded-lg shadow-md">
+      <Button
+        className={clsx(
+          baseButtonStyles,
+          "bg-yellow-300 hover:bg-yellow-600 text-black"
+        )}>
         <Image
           src={kakaoLogo}
           width={35}
@@ -50,7 +51,11 @@ export default function LoginButtons() {
         <span className="text-lg font-medium">카카오 로그인</span>
       </Button>
 
-      <Button className="w-80 h-16 bg-green-500 hover:bg-green-600 text-white relative rounded-lg shadow-md">
+      <Button
+        className={clsx(
+          baseButtonStyles,
+          "bg-green-500 hover:bg-green-600 text-white"
+        )}>
         <SiNaver size={30} className="absolute left-4" />
         <span className="text-lg font-medium">네이버 로그인</span>
       </Button>
---
+++ src/styles/styles.ts
@@ -0,0 +1,2 @@
+export const baseButtonStyles =
+  "w-80 h-16 relative rounded-lg shadow-md text-lg font-medium flex items-center justify-center";
