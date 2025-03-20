f7241a248d2039a8ee832892569040d3af052aa5
현지현 <qwere4585@naver.com>
2025-03-20 18:06:30 +0900

    feat: 메인페이지 고양이 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,21 @@ import BottomMenu from "@/components/main/BottomMenu";
 import ProgressBar from "@/components/main/ProgressBar";
 import TopMenu from "@/components/main/TopMenu";
 import NavigationBar from "@/components/ui/NavigationBar";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className= "min-h-screen">
+    <div className="flex flex-col min-h-screen">
       <TopMenu />
-      <ProgressBar now={35}/>
+      <ProgressBar now={35} />
+      <div className="flex justify-center py-25 sm:py-15 md:py-10">
+        <Image
+          src={"/assets/images/cat.svg"}
+          alt="cat"
+          width={180}
+          height={180}
+        />
+      </div>
       <BottomMenu />
       <NavigationBar />
     </div>
