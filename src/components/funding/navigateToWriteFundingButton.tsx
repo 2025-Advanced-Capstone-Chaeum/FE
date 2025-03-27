f21426464910e7737da7415b1d35b1211c51b679
aeri0730 <dodl8549@gmail.com>
2025-03-27 21:24:07 +0900

    refactor: 절대경로로 변경 & 등록하기버튼 수정

--- src/components/funding/navigateToWriteFundingButton.tsx
+++ src/components/funding/navigateToWriteFundingButton.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
 
 const NavigateToWriteFundingButton = () => {
+  const router = useRouter();
   return (
-    <div>NavigateToWriteFundingButton</div>
-  )
-}
+    <Button
+      className="bg-primary text-white text-base font-semibold rounded-2xl"
+      onClick={() => {
+        router.push("/funding/write");
+      }}>
+      {" "}
+      등록하기
+    </Button>
+  );
+};
 
-export default NavigateToWriteFundingButton
+export default NavigateToWriteFundingButton;
