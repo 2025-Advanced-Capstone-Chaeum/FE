5b47387c990cf5c671222416a48a2294fa353277
aeri0730 <dodl8549@gmail.com>
2025-03-27 19:38:44 +0900

    feature : 펀딩 상세 페이지 구현 ( #25) , 펀딩리스트페이지에서 등록하기 버튼클릭해서 펀딩생성페이지로 이동

---
+++ src/components/funding/navigateToWriteFundingButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavigateToWriteFundingButton = () => {
+  return (
+    <div>NavigateToWriteFundingButton</div>
+  )
+}
+
+export default NavigateToWriteFundingButton
