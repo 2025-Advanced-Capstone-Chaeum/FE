aca28cbdac9975692a63198e1517e168de7ce6ff
현지현 <qwere4585@naver.com>
2025-03-28 16:38:07 +0900

    feat: 충전 수단 선택 컴포넌트 생성

---
+++ src/components/payment/SelectPayMethod.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import React from "react";
+
+const SelectPayMethod = () => {
+  return (
+    <div className="flex flex-col px-10 gap-5">
+      <span className="flex text-sm">충전 수단 선택</span>
+      <div className="flex w-full justify-between px-5">
+        <Image
+          src={"/assets/images/kakao-pay.png"}
+          alt="KakaoPay"
+          className="cursor-pointer"
+          width={120}
+          height={40}
+        />
+        <div className="flex justify-center items-center w-33 h-13 bg-white rounded-3xl">
+          <Image
+            src={"/assets/images/toss-pay.png"}
+            alt="TossPay"
+            className="cursor-pointer"
+            width={115}
+            height={40}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SelectPayMethod;
