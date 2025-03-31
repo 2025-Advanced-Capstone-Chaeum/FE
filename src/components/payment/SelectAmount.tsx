6fa1cb7796becd1981109c5e791666384f0497e1
현지현 <qwere4585@naver.com>
2025-03-31 22:35:50 +0900

    feat: 버튼 클릭 이벤트 추가

--- src/components/payment/SelectAmount.tsx
+++ src/components/payment/SelectAmount.tsx
@@ -1,36 +1,44 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Button } from "../ui/button";
 
 const SelectAmount = () => {
+  const [isActiveAmount, setIsActiveAmount] = useState<number>(0);
+  const amountItems = [
+    { id: 1, label: "1000원" },
+    { id: 2, label: "3000원" },
+    { id: 3, label: "5000원" },
+    { id: 4, label: "10000원" },
+    { id: 5, label: "30000원" },
+    { id: 6, label: "50000원" },
+  ];
+
+  const handleAmountClick = (id: number) => setIsActiveAmount(id);
+
   return (
-    <div>
-      <div className="flex flex-col gap-3 px-12 sm:pt-3 sm:px-22 md:pt-5 md:px-42">
-        <span className="text-sm">기부 금액 선택</span>
-        <div className="flex justify-center items-center gap-2">
-          <Button variant="soft" className="w-[150px] py-5">
-            1000원
-          </Button>
-          <Button variant="soft" className="w-[150px] py-5">
-            3000원
-          </Button>
+    <div className="px-12 sm:pt-3 sm:px-22 md:pt-5 md:px-42">
+      <span className="text-sm">기부 금액 선택</span>
+      {[0, 2, 4].map((start) => (
+        <div key={start} className="flex justify-center items-center gap-2">
+          {amountItems.slice(start, start + 2).map((item) => (
+            <div
+              key={item.id}
+              className={`flex flex-col items-center justify-center relative ${
+                isActiveAmount === item.id ? "opacity-50" : "opacity-100"
+              }`}
+            >
+              <Button
+                variant="soft"
+                className="w-[150px] py-5 my-1.5"
+                onClick={() => handleAmountClick(item.id)}
+              >
+                {item.label}
+              </Button>
+            </div>
+          ))}
         </div>
-        <div className="flex justify-center items-center gap-2">
-          <Button variant="soft" className="w-[150px] py-5">
-            5000원
-          </Button>
-          <Button variant="soft" className="w-[150px] py-5">
-            10000원
-          </Button>
-        </div>
-        <div className="flex justify-center items-center gap-2">
-          <Button variant="soft" className="w-[150px] py-5">
-            30000원
-          </Button>
-          <Button variant="soft" className="w-[150px] py-5">
-            50000원
-          </Button>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
