a9c53306481b0582bd238780b97d0622182759be
현지현 <qwere4585@naver.com>
2025-04-01 00:15:06 +0900

    refactor: 카카오페이 버튼 커스텀으로 구현

--- src/components/payment/SelectPayMethod.tsx
+++ src/components/payment/SelectPayMethod.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import React, { useState } from "react";
@@ -13,23 +13,39 @@ const SelectPayMethod = () => {
   return (
     <div className="flex flex-col px-10 gap-5">
       <span className="flex text-sm">충전 수단 선택</span>
-      <div className="flex w-full justify-between px-5 sm:px-15 md:px-35">
-        <Image
-          src={"/assets/images/kakao-pay.png"}
-          alt="KakaoPay"
-          className={`h-11 cursor-pointer ${activePayMethod === 1 ? 'opacity-50' : 'opacity-100'}`}
-          width={120}
-          height={40}
+      <div className="flex w-full justify-between px-3 sm:px-13 md:px-30">
+        <div
+          className={`flex justify-center items-center w-33 h-11 px-3 gap-0.5 bg-[#FEE500] rounded-3xl cursor-pointer ${
+            activePayMethod === 1 ? "opacity-50" : "opacity-100"
+          }`}
           onClick={() => handlePayMethodClick(1)}
-        />
-        <div className="flex justify-center items-center w-33 h-11 bg-white rounded-3xl">
+        >
+          <Image
+            src={"/assets/images/kakao-logo.png"}
+            alt="KakaoPay"
+            className="h-6"
+            width={24}
+            height={24}
+          />
+          <Image
+            src={"/assets/images/kakao-pay-logo.png"}
+            alt="KakaoPay"
+            className="h-5.5"
+            width={105}
+            height={20}
+          />
+        </div>
+        <div
+          className={`flex justify-center items-center w-33 h-11 bg-white rounded-3xl cursor-pointer ${
+            activePayMethod === 2 ? "opacity-50" : "opacity-100"
+          }`}
+          onClick={() => handlePayMethodClick(2)}
+        >
           <Image
             src={"/assets/images/toss-pay.png"}
             alt="TossPay"
-            className={`cursor-pointer ${activePayMethod === 2 ? 'opacity-50' : 'opacity-100'}`}
             width={115}
             height={40}
-            onClick={() => handlePayMethodClick(2)}
           />
         </div>
       </div>
