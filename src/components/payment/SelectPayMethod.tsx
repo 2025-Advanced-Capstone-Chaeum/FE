00c643c66d9ac85aff74a490a78052d15e54f861
현지현 <qwere4585@naver.com>
2025-03-31 22:53:23 +0900

    feat: 충전 수단 클릭 이벤트 추가

--- src/components/payment/SelectPayMethod.tsx
+++ src/components/payment/SelectPayMethod.tsx
@@ -1,7 +1,15 @@
+'use client'
+
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 const SelectPayMethod = () => {
+  const [activePayMethod, setActivePayMethod] = useState<number>(0);
+
+  const handlePayMethodClick = (id: number) => {
+    setActivePayMethod(id);
+  };
+
   return (
     <div className="flex flex-col px-10 gap-5">
       <span className="flex text-sm">충전 수단 선택</span>
@@ -9,17 +17,19 @@ const SelectPayMethod = () => {
         <Image
           src={"/assets/images/kakao-pay.png"}
           alt="KakaoPay"
-          className="cursor-pointer"
+          className={`cursor-pointer ${activePayMethod === 1 ? 'opacity-50' : 'opacity-100'}`}
           width={120}
           height={40}
+          onClick={() => handlePayMethodClick(1)}
         />
         <div className="flex justify-center items-center w-33 h-13 bg-white rounded-3xl">
           <Image
             src={"/assets/images/toss-pay.png"}
             alt="TossPay"
-            className="cursor-pointer"
+            className={`cursor-pointer ${activePayMethod === 2 ? 'opacity-50' : 'opacity-100'}`}
             width={115}
             height={40}
+            onClick={() => handlePayMethodClick(2)}
           />
         </div>
       </div>
