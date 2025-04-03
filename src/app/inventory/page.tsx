b1c2cf957466d66426b44dafb6f52dc944129690
현지현 <qwere4585@naver.com>
2025-04-03 16:33:29 +0900

    feat: 아이템 컨테이너 생성

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -1,17 +1,26 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e115865 (feat: 아이템 컨테이너 생성)
 import BackButton from "@/components/BackButton";
 import ItemContainer from "@/components/inventory/ItemContainer";
 import Image from "next/image";
 import React from "react";
+<<<<<<< HEAD
 =======
 import BackButton from '@/components/BackButton'
 import React from 'react'
 >>>>>>> 3ff5ee2 (feat: 뒤로가기 버튼에 children 추가)
+=======
+>>>>>>> e115865 (feat: 아이템 컨테이너 생성)
 
 const InventoryPage = () => {
   return (
     <div>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e115865 (feat: 아이템 컨테이너 생성)
       <BackButton>인벤토리</BackButton>
       <div className="flex justify-center py-14">
         <Image
@@ -22,6 +31,7 @@ const InventoryPage = () => {
         />
       </div>
       <ItemContainer />
+<<<<<<< HEAD
     </div>
   );
 };
@@ -29,9 +39,15 @@ const InventoryPage = () => {
 export default InventoryPage;
 =======
         <BackButton>인벤토리</BackButton>
+=======
+>>>>>>> e115865 (feat: 아이템 컨테이너 생성)
     </div>
-  )
-}
+  );
+};
 
+<<<<<<< HEAD
 export default InventoryPage
 >>>>>>> 3ff5ee2 (feat: 뒤로가기 버튼에 children 추가)
+=======
+export default InventoryPage;
+>>>>>>> e115865 (feat: 아이템 컨테이너 생성)
