bb1aba81294aca25ebe681cfdbab086d502c35d4
aeri0730 <dodl8549@gmail.com>
2025-04-03 23:40:04 +0900

    refactor : pr 리뷰 수정 (#54)

--- src/app/(review)/review/[id]/page.tsx
+++ src/app/(review)/review/[id]/page.tsx
@@ -26,8 +26,10 @@ export default function ReviewDetailPage() {
       <BackButton />
       <div className="flex flex-col items-center px-2 text-secondary space-y-8">
         {/* 리뷰 카드 */}
-        <div className="w-80 rounded-xl bg-white p-4 flex flex-col items-center my-8 shadow-md">
-          <p className="text-center text-sm mb-3">이 게시글에 대한 리뷰입니다.</p>
+        <div className="w-80 rounded-xl bg-white p-3 flex flex-col items-center my-6 shadow-md gap-y-2">
+          <p className="text-center text-sm mb-3">
+            이 게시글에 대한 리뷰입니다.
+          </p>
           <Image
             src="/assets/images/study.png"
             alt="공부하는 모습"
@@ -39,10 +41,12 @@ export default function ReviewDetailPage() {
         </div>
 
         {/* 펀딩 후기 섹션 */}
-        <div className="flex flex-col items-center w-80 space-y-5">
+        <div className="flex flex-col items-center w-80 space-y-8">
           {/* 제목 */}
           <div className="w-full border-b-2 border-gray-500 pb-2">
-            <p className="text-lg text-secondary text-center">감격스러운 펀딩 완료 후기</p>
+            <p className="text-lg text-secondary text-center">
+              감격스러운 펀딩 완료 후기
+            </p>
           </div>
 
           {/* 이미지 */}
@@ -56,7 +60,10 @@ export default function ReviewDetailPage() {
 
           {/* 메시지 박스 */}
           <div className="bg-white p-5 rounded-xl shadow-md w-full h-auto text-center">
-            <p>덕분에 필요한 물건을 구매할 수 있었습니다! 도와주신 분들 모두 감사합니다!!</p>
+            <p>
+              덕분에 필요한 물건을 구매할 수 있었습니다! 도와주신 분들 모두
+              감사합니다!!
+            </p>
           </div>
         </div>
       </div>
