3ff5ee2dada6509fc613f2f768e2863d8e248afb
현지현 <qwere4585@naver.com>
2025-04-03 10:37:03 +0900

    feat: 뒤로가기 버튼에 children 추가

---
+++ src/app/inventory/page.tsx
@@ -0,0 +1,12 @@
+import BackButton from '@/components/BackButton'
+import React from 'react'
+
+const InventoryPage = () => {
+  return (
+    <div>
+        <BackButton>인벤토리</BackButton>
+    </div>
+  )
+}
+
+export default InventoryPage
