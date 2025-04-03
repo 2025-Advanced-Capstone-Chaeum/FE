bc01163662960567acd3ccf2cbeb40ca4325ad29
현지현 <qwere4585@naver.com>
2025-04-03 10:37:03 +0900

    feat: 뒤로가기 버튼에 children 추가

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -1,11 +1,17 @@
+<<<<<<< HEAD
 import BackButton from "@/components/BackButton";
 import ItemContainer from "@/components/inventory/ItemContainer";
 import Image from "next/image";
 import React from "react";
+=======
+import BackButton from '@/components/BackButton'
+import React from 'react'
+>>>>>>> 3ff5ee2 (feat: 뒤로가기 버튼에 children 추가)
 
 const InventoryPage = () => {
   return (
     <div>
+<<<<<<< HEAD
       <BackButton>인벤토리</BackButton>
       <div className="flex justify-center py-14">
         <Image
@@ -21,3 +27,11 @@ const InventoryPage = () => {
 };
 
 export default InventoryPage;
+=======
+        <BackButton>인벤토리</BackButton>
+    </div>
+  )
+}
+
+export default InventoryPage
+>>>>>>> 3ff5ee2 (feat: 뒤로가기 버튼에 children 추가)
