af42658774d3ab655c3ecda1abcf069cfa281ede
aeri0730 <dodl8549@gmail.com>
2025-04-08 21:46:08 +0900

    style : 기부자용 프로필페이지 UI 구현 (#58)

---
+++ src/lib/myDonationList.ts
@@ -0,0 +1,74 @@
+import { randomUUID } from "crypto";
+
+export const myDonationList = [
+  {
+    id: randomUUID(),
+    title: "공부해서 사회에 보답하겠습니다.",
+    date: new Date("2025-04-08T15:30:25"),
+    fundingPayment: 40000,
+    photo: "",
+  },
+  {
+    id: randomUUID(),
+    title: "동생이 내일 생일이에요.",
+    date: new Date("2025-04-04T11:10:25"),
+    fundingPayment: 60000,
+    photo: "",
+  },
+  {
+    id: randomUUID(),
+    title: "공부해서 사회에 보답하겠습니다.",
+    date: new Date("2025-04-08T15:30:25"),
+    fundingPayment: 40000,
+    photo: "",
+  },
+  {
+    id: randomUUID(),
+    title: "동생이 내일 생일이에요.",
+    date: new Date("2025-04-04T11:10:25"),
+    fundingPayment: 60000,
+    photo: "",
+  },
+  {
+    id: randomUUID(),
+    title: "공부해서 사회에 보답하겠습니다.",
+    date: new Date("2025-04-08T15:30:25"),
+    fundingPayment: 40000,
+    photo: "",
+  },
+  {
+    id: randomUUID(),
+    title: "동생이 내일 생일이에요.",
+    date: new Date("2025-04-04T11:10:25"),
+    fundingPayment: 60000,
+    photo: "",
+  },
+  {
+    id: randomUUID(),
+    title: "공부해서 사회에 보답하겠습니다.",
+    date: new Date("2025-04-08T15:30:25"),
+    fundingPayment: 40000,
+    photo: "",
+  },
+  {
+    id: randomUUID(),
+    title: "동생이 내일 생일이에요.",
+    date: new Date("2025-04-04T11:10:25"),
+    fundingPayment: 60000,
+    photo: "",
+  },
+  {
+    id: randomUUID(),
+    title: "공부해서 사회에 보답하겠습니다.",
+    date: new Date("2025-04-08T15:30:25"),
+    fundingPayment: 40000,
+    photo: "",
+  },
+  {
+    id: randomUUID(),
+    title: "동생이 내일 생일이에요.",
+    date: new Date("2025-04-04T11:10:25"),
+    fundingPayment: 60000,
+    photo: "",
+  },
+];
