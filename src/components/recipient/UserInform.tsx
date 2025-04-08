b24397cd732be4e1ad7fa8442da3511e295a0d8c
현지현 <qwere4585@naver.com>
2025-04-08 20:43:03 +0900

    feat: 수혜자정보 컴포넌트 생성

---
+++ src/components/recipient/UserInform.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import React from 'react'
+
+const UserInform = () => {
+  return (
+    <div>
+        <div className='flex flex-col justify-center items-center gap-4'>
+            <Image src="/assets/icons/woman-profile.png" alt="WomanProfile" width={100} height={100} />
+            <div className='flex flex-col text-secondary opacity-80 justify-center items-center'>
+                <span>수혜자</span>
+                <span className='text-sm'>suhyeja@gmail.com</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default UserInform
