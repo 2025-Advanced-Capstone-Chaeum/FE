5ca124d98d57d5ebcacd7be5e433c294e163fd2a
aeri0730 <dodl8549@gmail.com>
2025-05-15 14:45:09 +0900

    api : api 연동 완료, data값받아와서 id, level, experiencePoint, levelUpPercentage 값중 화면에 출력되는 값 leve, experiencePoint사용 (#101)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,28 @@ import ProgressBar from "@/components/main/ProgressBar";
 import TopMenu from "@/components/main/TopMenu";
 import NavigationBar from "@/components/ui/NavigationBar";
 import axiosInstance from "@/lib/api/axios";
+import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
   const fetchCatInfo = async () => {
-    const res = await axiosInstance.get("/api/v1/cat", {
-      withCredentials: true, // HttpOnly 쿠키를 전송하려면 필수
-    });
+    const { data } = await axiosInstance.get("/api/v1/cat");
+    console.log(data.data);
+    return data.data;
   };
+
+  const { data: catInfo } = useQuery({
+    queryKey: ["cat"],
+    queryFn: fetchCatInfo,
+  });
 
   return (
     <div className="flex flex-col h-screen">
       <TopMenu />
       <div className="flex flex-col justify-between flex-grow pb-[8rem]">
         <div className="flex flex-col py-5">
-          <ProgressBar now={35} />
+          {catInfo && <ProgressBar catData={catInfo} />}
         </div>
         <div className="flex justify-center">
           <Image
