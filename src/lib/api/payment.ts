26966cc54ec832172af1bf0238deedffff06a7f5
현지현 <qwere4585@naver.com>
2025-05-20 23:53:32 +0900

    feat: 기부, 결제 생성 호출 딜레이 추가

--- src/lib/api/payment.ts
+++ src/lib/api/payment.ts
@@ -3,6 +3,7 @@ import axiosInstance from "./axios";
 export interface PaymentCreateData {
   donationId?: number;
   amount: number;
+  points: number;
   transactionId: string;
   paymentMethod: string;
   status: string;
@@ -15,6 +16,8 @@ export interface PaymentCreateData {
 export const createPayment = async (
     paymentData: PaymentCreateData
 ) : Promise<PaymentCreateData> => {
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
   const response = await axiosInstance.post("/api/v1/payment", paymentData);
   return response.data;
 };
