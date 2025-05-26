dcfd2c14a925122ad29000da7de186db01a82126
aeri0730 <dodl8549@gmail.com>
2025-05-27 02:19:52 +0900

    refactor : token 지우기

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -3,9 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "https://api.chaeum.site",
   withCredentials: true, // <- HttpOnly 쿠키 전송
-  headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJjYXRlZ29yeSI6ImFjY2VzcyIsImVtYWlsIjoiZG9mbDg1NDlAbmF2ZXIuY29tIiwibWVtYmVySWQiOjcsInJvbGUiOiJET05PUiIsImlhdCI6MTc0ODI3Njg0MiwiZXhwIjoxNzQ4MjgwNDQyfQ.xaP1uH2j50geMBPRgXSX3IcTu42e_79CYdugyaxjn_i9exGm1xb0VQyVBVDGPlf9HH1E2ezFc0riTxoYU56qlQ`,
-  },
+
 });
 
 export default axiosInstance;
