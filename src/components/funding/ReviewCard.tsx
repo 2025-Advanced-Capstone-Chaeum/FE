f4cbcd478354d0469ed12d5d92c6d3cf351719da
현지현 <qwere4585@naver.com>
2025-05-28 14:16:44 +0900

    refactor: 펀딩카드 이미지 null경우 처리

--- src/components/funding/ReviewCard.tsx
+++ src/components/funding/ReviewCard.tsx
@@ -1,21 +1,12 @@
-// components/funding/ReviewCard.tsx
-
 import React from "react";
 import Image from "next/image";
 import { useFunding } from "@/hooks/useFunding";
 
-type reviewDataProps = {
-  imageUrls: string[];
-  title: string;
-  content: string;
-};
-
 type ReviewCardProps = {
-  reviewData: reviewDataProps;
   fundingId?: number;
 };
 
-const ReviewCard = ({ reviewData, fundingId }: ReviewCardProps) => {
+const ReviewCard = ({ fundingId }: ReviewCardProps) => {
   const { fundingQuery } = useFunding({ fundingId: fundingId });
   const {
     data: fundingDetail,
@@ -23,13 +14,13 @@ const ReviewCard = ({ reviewData, fundingId }: ReviewCardProps) => {
     isError: isFundingError,
     error: fundingError,
   } = fundingQuery;
-  
-  const displayImageUrl: string =
-    (fundingDetail?.fundingImages && fundingDetail.fundingImages.length > 0)
-      ? fundingDetail.fundingImages[0].fileUrl
-      : reviewData.imageUrls[0];
 
-  const displayTitle = fundingDetail?.title || reviewData.title;
+  const displayImageUrl: string | null =
+    fundingDetail?.fundingImages && fundingDetail.fundingImages.length > 0
+      ? fundingDetail.fundingImages[0].fileUrl
+      : null;
+
+  const displayTitle = fundingDetail?.title;
 
   if (isFundingPending) {
     return (
@@ -46,13 +37,19 @@ const ReviewCard = ({ reviewData, fundingId }: ReviewCardProps) => {
 
   return (
     <div className="flex flex-col pt-6">
-      <Image
-        src={displayImageUrl}
-        alt={displayTitle || "펀딩 프로젝트"}
-        width={150}
-        height={100}
-        className="w-full h-auto rounded-lg object-cover"
-      />
+      {displayImageUrl ? (
+        <Image
+          src={displayImageUrl}
+          alt={displayTitle || "펀딩 프로젝트"}
+          width={150}
+          height={100}
+          className="w-full h-auto rounded-lg object-cover"
+        />
+      ) : (
+        <div className="w-full h-[100px] bg-gray-100 rounded-lg flex items-center justify-center text-gray-400">
+          펀딩 이미지가 없습니다.
+        </div>
+      )}
       <p className="text-sm text-secondary mt-4 text-left">
         {displayTitle && displayTitle.length > 13
           ? displayTitle.slice(0, 13) + ".."
