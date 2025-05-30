463d2fd98d1ba1387886bd3c40e08f4b1d018b26
aeri0730 <dodl8549@gmail.com>
2025-05-31 03:03:37 +0900

    refactor : 토근 지우기 (#128)

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -3,10 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: "https://api.chaeum.site",
   withCredentials: true, // <- HttpOnly 쿠키 전송
-  headers: {
-    Authorization:
-      "Bearer eyJhbGciOiJIUzUxMiJ9.eyJjYXRlZ29yeSI6ImFjY2VzcyIsImVtYWlsIjoiZG9mbDg1NDlAbmF2ZXIuY29tIiwibWVtYmVySWQiOjgsInJvbGUiOiJSRUNJUElFTlQiLCJpYXQiOjE3NDg2MjQ0NDEsImV4cCI6MTc0ODYyODA0MX0.rdqYmHztTMgCUokUdElQQfzBz8-GqCjmM4BSs30OdnH5d7ydLYU99YJZ8kE6ZY6bwc1RVy0Hwn6WRFgcBz58Sg",
-  },
 });
 
 export default axiosInstance;
