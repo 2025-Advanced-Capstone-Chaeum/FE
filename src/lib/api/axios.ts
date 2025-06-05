aea6cee8d33c89f62929c2b19c69489cd22ac62a
aeri0730 <dodl8549@gmail.com>
2025-06-06 03:16:46 +0900

    refactor : 토큰 지우기..

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -3,10 +3,6 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "https://api.chaeum.site",
   withCredentials: true, // <- HttpOnly 쿠키 전송
-  headers: {
-    Authorization:
-      "Bearer eyJhbGciOiJIUzUxMiJ9.eyJjYXRlZ29yeSI6ImFjY2VzcyIsImVtYWlsIjoiZG9mbDg1NDlAbmF2ZXIuY29tIiwibWVtYmVySWQiOjIsInJvbGUiOiJET05PUiIsImlhdCI6MTc0OTE0NjA4NywiZXhwIjoxNzQ5MTQ5Njg3fQ.rl-tjGAzFkg2BseeCfrWWRUXsByx4wgWrhZhiCqiNPXtxR9YQ1-65qbRvgAklYF-amoyZjZHmfiXrnXaeoweMQ",
-  },
 });
 
 export default axiosInstance;
